8f19efff7d911babb6b721bd2cf00389a4679f70
Osvaldo Q. <ozald8600@gmail.com>
2025-07-31 00:34:15 -0400

    Beta Dialogue Manager System Completed
    - Added Dialogue Manager
    - Added Subtitles
    - Added EventBehaviorScripts to run a dialogue option ends
    
    Now all we need to do is play audio and the core gameplay loop is basically done.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[Serializable]
+public struct DialogueEvent
+{
+    public string text;
+    public float duration;
+    public EventBehaviorScript scriptToRunAtEnd;
+}
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI subtitles;
+    public bool playOnStart = true;
+    public List<DialogueEvent> dialogueQueue = new List<DialogueEvent>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playOnStart)
+        {
+            StartCoroutine("PlayDialogueQueue");
+        }
+    }
+
+    public IEnumerator PlayDialogueQueue()
+    {
+        foreach (DialogueEvent dialogueEvent in dialogueQueue)
+        {
+            subtitles.text = dialogueEvent.text;
+            yield return new WaitForSeconds(dialogueEvent.duration);
+            if (dialogueEvent.scriptToRunAtEnd != null)
+            {
+                dialogueEvent.scriptToRunAtEnd.StartEvent();
+            }
+        }
+
+        subtitles.text = "";
+    }
+}
