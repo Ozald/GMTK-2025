3eb436cd6d30039c830a265077ac5ddfd4ed01b4
Osvaldo Q. <ozald8600@gmail.com>
2025-07-31 15:43:39 -0400

    Gimmick 18

---
+++ Assets/Scripts/Gimmick-Specific/G18DestroyAd.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G18DestroyAd : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
