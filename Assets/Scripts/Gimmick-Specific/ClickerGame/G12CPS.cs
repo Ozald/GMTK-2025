243c1b3abe1e558a34e93336c6130445ae1b40d1
Reddyboi85 <83redknights@gmail.com>
2025-08-01 11:30:02 -0400

    G12 functionality

---
+++ Assets/Scripts/Gimmick-Specific/ClickerGame/G12CPS.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class G12CPS : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float purchaseAmount;
+    public G12clicking clickerScript;
+    public TextMeshProUGUI text;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Cost of upgrade: " + purchaseAmount;
+    }
+
+    private void OnMouseDown()
+    {
+        if (clickerScript.totalPoints > purchaseAmount)
+        {
+            clickerScript.totalPoints -= purchaseAmount;
+            purchaseAmount += 20;
+            clickerScript.clickPerSec++;
+        }
+    }
+}
