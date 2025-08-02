29d3127defadc91aaf1d02577e3c3372d2aa29c5
Osvaldo Q. <ozald8600@gmail.com>
2025-08-02 02:10:16 -0400

    Finalized Cutscenes and Death Effects

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    public float duration = 0.5f;
-    public float magnitude = 5f;
+    public float defaultDuration = 0.5f;
+    public float defaultMagnitude = 0.3f;
 
     public void StartShake()
     {
         StartCoroutine(Shake());
     }
 
-    IEnumerator Shake()
+    public void StartShake(float duration, float magnitude)
+    {
+        StartCoroutine(Shake(duration, magnitude));
+    }
+
+    IEnumerator Shake(float duration, float magnitude)
     {
         Vector3 cameraOriginalPos = transform.localPosition;
 
@@ -22,6 +27,27 @@ public class CameraShake : MonoBehaviour
         {
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(cameraOriginalPos.x + x, cameraOriginalPos.y + y, cameraOriginalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = cameraOriginalPos;
+    }
+
+    IEnumerator Shake()
+    {
+        Vector3 cameraOriginalPos = transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < defaultDuration)
+        {
+            float x = Random.Range(-1f, 1f) * defaultMagnitude;
+            float y = Random.Range(-1f, 1f) * defaultMagnitude;
 
             transform.localPosition = new Vector3(cameraOriginalPos.x + x, cameraOriginalPos.y + y, cameraOriginalPos.z);
 
