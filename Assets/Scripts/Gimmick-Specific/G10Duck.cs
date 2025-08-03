454d33f5fa13052dba1f01ef7da79ad2f58134db
Reddyboi85 <83redknights@gmail.com>
2025-08-03 00:47:09 -0400

    Ads added

--- Assets/Scripts/Gimmick-Specific/G10Duck.cs
+++ Assets/Scripts/Gimmick-Specific/G10Duck.cs
@@ -8,14 +8,17 @@ public class G10Duck : EventBehaviorScript
     public float speed;
     public bool eventStarted = false;
     public GameObject duck;
+    public AudioSource audioSource;
     public override void StartEvent()
     {
+        audioSource.Play();
         eventStarted = true;
     }
     void Update()
     {
         if (eventStarted)
         {
+            
             if (endPoint.position != duck.transform.position)
             {
                 float step = speed * Time.deltaTime;
--- Assets/Scripts/Gimmick-Specific/ManyDucks.cs
+++ Assets/Scripts/Gimmick-Specific/ManyDucks.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathAudio : MonoBehaviour
+public class ManyDucks : MonoBehaviour
 {
-
-    
-    public PlayerController playerController;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
 
+        if (Random.Range(1,50) == 1)
+        {
+            audioSource.Play();
+        }
     }
 }
