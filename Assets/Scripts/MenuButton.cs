01d2052f803ffa8192fc55283e53edd109bed1e3
Reddyboi85 <83redknights@gmail.com>
2025-08-02 21:27:13 -0400

    Working menu

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    
+    public void PlayButtonClick()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
